33edb8707ca6abd08043ed4adef72648875c5ddd
Ivan Poberezhnyuk <vanyapoberejnuk@gmail.com>
2019-06-27 17:42:09 +0300

    Updates

---
+++ precache-manifest.ae6f7d18e7ab4febb07084a150fb45ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7545f105831b52cd41fa908499202f05",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "11c901a4cb1e7d1b05c4",
+    "url": "/react_dynamic-list-of-posts/static/css/main.c385f3ee.chunk.css"
+  },
+  {
+    "revision": "ec5157db6e8341347375",
+    "url": "/react_dynamic-list-of-posts/static/js/2.bf521901.chunk.js"
+  },
+  {
+    "revision": "11c901a4cb1e7d1b05c4",
+    "url": "/react_dynamic-list-of-posts/static/js/main.80a81686.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  },
+  {
+    "revision": "647b23e62de87fd2d6376c8cf748add6",
+    "url": "/react_dynamic-list-of-posts/static/media/location_ico.647b23e6.svg"
+  }
+]);
